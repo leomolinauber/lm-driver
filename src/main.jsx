4b53beb3dd6ec0b9478d61a960ddc1c1f655c286
leomolinauber <leonardooliveiramolina@outlook.com>
2025-04-10 21:05:40 -0300

    Adiciona ponto de entrada com main.jsx e index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import LMDriverCalculator from './LMDriverCalculator'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LMDriverCalculator from './LMDriverCalculator';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LMDriverCalculator />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
